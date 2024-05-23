cf4c90a115acdd6b99a4a40c7ca875ec02bf7486
furkansakizci <fursakizci@gmail.com>
2024-05-23 20:49:56 +1000

    cache control

--- src/app/core/services/todo-service.service.ts
+++ src/app/core/services/todo-service.service.ts
@@ -2,36 +2,57 @@ import { Injectable, inject } from '@angular/core';
 import { Task, TaskStatus } from '../models/Task';
 import { Observable } from 'rxjs';
 import { apiEndpoint } from '../constants/constants';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  http = inject(HttpClient)
+  http = inject(HttpClient);
+
+  private createNoCacheHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+      'Pragma': 'no-cache',
+      'Expires': '0'
+    });
+  }
 
   addTask(data: Task): Observable<Task> {
-    return this.http.post<Task>
-      (`${apiEndpoint.TodoEndpoint.addTodo}`, data);
+    return this.http.post<Task>(
+      `${apiEndpoint.TodoEndpoint.addTodo}`, 
+      data,
+      { headers: this.createNoCacheHeaders() }
+    );
   }
 
   getAllTasks(): Observable<Task[]> {
-    return this.http.get<Task[]>
-    (`${apiEndpoint.TodoEndpoint.getAllTodo}`);
+    return this.http.get<Task[]>(
+      `${apiEndpoint.TodoEndpoint.getAllTodo}`,
+      { headers: this.createNoCacheHeaders() }
+    );
   }
 
   getAllTasksByStatus(status: TaskStatus): Observable<Task[]> {
     return this.http.get<Task[]>(
-      `${apiEndpoint.TodoEndpoint.getTodosByStatus}/${status}`);
+      `${apiEndpoint.TodoEndpoint.getTodosByStatus}/${status}`,
+      { headers: this.createNoCacheHeaders() }
+    );
   }
 
   deleteTask(id: string): Observable<Task> {
-    return this.http.delete<Task>(`${apiEndpoint.TodoEndpoint.deleteTodo}/${id}`);
+    return this.http.delete<Task>(
+      `${apiEndpoint.TodoEndpoint.deleteTodo}/${id}`,
+      { headers: this.createNoCacheHeaders() }
+    );
   }
 
   updateTask(data: Task): Observable<Task> {
-    return this.http.put<Task>
-      (`${apiEndpoint.TodoEndpoint.updateTodo}`, data);
+    return this.http.put<Task>(
+      `${apiEndpoint.TodoEndpoint.updateTodo}`, 
+      data,
+      { headers: this.createNoCacheHeaders() }
+    );
   }
 }
